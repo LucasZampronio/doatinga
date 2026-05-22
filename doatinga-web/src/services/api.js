8dbea19dba117ad744402f733dbb1a3006a9935c
Lucas Zampronio <lucas.zampronio.silva@gmail.com>
2026-05-22 18:31:49 -0300

    Fix: update API baseURL to public IP for production

--- doatinga-web/src/services/api.js
+++ doatinga-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3005/api',
+    baseURL: 'http://163.176.226.58/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
